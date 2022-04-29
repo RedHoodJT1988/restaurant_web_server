d3a84cb8bd90d191ce88e064592f753c5b0d7767
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2022-04-28 20:14:37 -0700

    added mvp for restaurant app

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import workingHours from "./data/workingHours.js";
+import menuItems from "./data/menuItem.js";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { name: `Welcome to What's Fare is Fair!` });
+});
+
+app.get("/menu", (req, res) => {
+  res.render("menu", { menuItems });
+});
+
+app.get("/hours", (req, res) => {
+  const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  res.render("hours", { workingHours, days });
+});
+
+app.listen(port, () => {
+  console.log(`Local server is listening on port: ${port}`);
+});
